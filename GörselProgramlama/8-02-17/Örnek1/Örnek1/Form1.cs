c71158bbe6d51926fd60891d072f358fb7fc79d5
Ceren Cömert <kodbilenkiz@gmail.com>
2017-02-08 22:48:42 +0300

    08-02-17 tarihli örnekler repoya eklendi.

---
+++ GörselProgramlama/8-02-17/Örnek1/Örnek1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Örnek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text=="")
+                MessageBox.Show("Yazı yok");
+            else
+            MessageBox.Show("Yazı: "+textBox1.Text);
+        }
+    }
+}
