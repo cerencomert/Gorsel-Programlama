6e4a97df9adb31045715230a1c9784e6a419e80f
Ceren Cömert <kodbilenkiz@gmail.com>
2017-02-08 23:28:48 +0300

    Kod çalışmaları eklendi.
    08-02-17 tarihli örneklere ait kişisel çalışmalar ve ödev  kodu eklendi.

---
+++ GörselProgramlama/8-02-17/Örnek2-Randomsayi/Örnek2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Örnek2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Random rnd = new Random();
+            int number = rnd.Next(1000);
+            label1.Text = number.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Mehmet") 
+                MessageBox.Show("Merhaba Mehmet");
+            else if (textBox1.Text == "Ahmet") 
+                MessageBox.Show("Merhaba Ahmet");
+            else if (textBox1.Text=="Ali")
+                MessageBox.Show("Merhaba Ali");
+            else MessageBox.Show("İsim farklı");
+            textBox1.Text = "";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string Ad = textBox2.Text;
+            textBox2.Text = "";
+            switch(Ad)
+            {
+                case "Ömer":
+                    MessageBox.Show(Ad+"İsim seçtiniz");
+                    break;
+                case "Mustafa":
+                    MessageBox.Show(Ad+"İsim seçtiniz");
+                    break;
+                default:
+                    MessageBox.Show("Seçtiğiniz isim listede yok.");
+                    break;
+            }
+        }
+    }
+}
---
+++ GörselProgramlama/8-02-17/Örnek3-Clickcounter/Örnek3/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Örnek3
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            for (int i=0; i<20; i+=3)
+            {
+                label1.Text += i.ToString() + " ";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            counter++;
+            int[] Dizi = new int[] { 0, 5, 9, 7, 8, 2, 9, 1, 34 };
+            if (counter % 2 == 0)
+            {
+                label2.Text = "";
+            }
+            else
+            {
+                foreach (int i in Dizi)
+                {
+                    label2.Text += i.ToString() + " ";
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (char karakter in textBox1.Text)
+            {
+                listBox1.Items.Add(karakter);
+            }
+            //listBox2.Items.Add(textBox1.Text);
+        }
+
+       
+    }
+}
